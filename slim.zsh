6911e637e423eabe331fb4ecbe40007148feeb76
Bartosz Pranczke <bartosz.pranczke@netguru.pl>
2014-06-21 13:20:34 +0200

    Add zsh-syntax-highlighting

--- slim.zsh
+++ slim.zsh
@@ -10,6 +10,7 @@ slim_path=`dirname $0`
 source $slim_path/keys.zsh
 source $slim_path/history.zsh
 source $slim_path/completion.zsh
+source $slim_path/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 if command -v fasd >/dev/null 2>&1; then
   eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install posix-alias)"
