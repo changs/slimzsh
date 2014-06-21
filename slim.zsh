c442e56036af5f0e6a950772ed221aac2c696ab4
Bartosz Pranczke <bartosz.pranczke@netguru.pl>
2014-06-21 12:36:56 +0200

    Completion configuration stolen from oh-my-zsh

--- slim.zsh
+++ slim.zsh
@@ -1,7 +1,11 @@
 autoload -U promptinit && promptinit
 prompt pure
 
+autoload -U compinit
+compinit
+
 slim_path=`dirname $0`
 source $slim_path/keys.zsh
 source $slim_path/history.zsh
+source $slim_path/completion.zsh
 
