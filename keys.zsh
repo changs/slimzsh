832d6362477f9adda582d0723a50ab53e2d4d17a
Bartosz Pranczke <bartosz.pranczke@netguru.pl>
2014-06-21 12:26:46 +0200

    Keys configuration

---
+++ keys.zsh
@@ -0,0 +1,23 @@
+bindkey -e
+bindkey '\ew' kill-region
+bindkey -s '\el' "ls\n"
+bindkey '^r' history-incremental-search-backward
+bindkey "^[[5~" up-line-or-history
+bindkey "^[[6~" down-line-or-history
+bindkey '^[[A' up-line-or-search
+bindkey '^[[B' down-line-or-search
+bindkey "^[[H" beginning-of-line
+bindkey "^[[1~" beginning-of-line
+bindkey "^[OH" beginning-of-line
+bindkey "^[[F"  end-of-line
+bindkey "^[[4~" end-of-line
+bindkey "^[OF" end-of-line
+bindkey ' ' magic-space
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
+bindkey '^[[Z' reverse-menu-complete
+bindkey '^?' backward-delete-char
+bindkey "^[[3~" delete-char
+bindkey "^[3;5~" delete-char
+bindkey "\e[3~" delete-char
+
--- slim.zsh
+++ slim.zsh
@@ -1,2 +1,6 @@
 autoload -U promptinit && promptinit
 prompt pure
+
+slim_path=`dirname $0`
+source $slim_path/keys.zsh
+
