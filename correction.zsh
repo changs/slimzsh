4bfecce97c87920a0b14e5c83be97b4708753476
Bartosz Pranczke <bartosz.pranczke@netguru.pl>
2014-07-11 17:03:12 +0200

    Add correction

---
+++ correction.zsh
@@ -0,0 +1,7 @@
+setopt correct_all
+alias man='nocorrect man'
+alias mv='nocorrect mv'
+alias mkdir='nocorrect mkdir'
+alias gist='nocorrect gist'
+alias sudo='nocorrect sudo'
+
--- slim.zsh
+++ slim.zsh
@@ -13,6 +13,7 @@ source $slim_path/keys.zsh
 source $slim_path/history.zsh
 source $slim_path/completion.zsh
 source $slim_path/aliases.zsh
+source $slim_path/correction.zsh
 source $slim_path/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 if command -v fasd >/dev/null 2>&1; then
