2d42ede5dcf023450b406d03b6fe1d23de3039ab
Adam Nowak <lubieniebieski@gmail.com>
2014-07-14 18:55:33 +0200

    add few helpful git aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,8 +1,15 @@
+# UTILITY
+
 alias less='less -R'
 alias grep='grep --color=always'
 alias ..='cd ../'
+
+# GIT
+
 alias gd='git diff'
 alias gco='git checkout'
-alias gpp='git pull; git push'
 alias gs='git status'
-
+alias gl='git pull'
+alias gp='git push'
+alias gpp='git pull; git push'
+alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
