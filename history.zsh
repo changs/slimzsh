d92f78c94afb85cf3652b3cc4cea10eefa4da652
Edward Lee <edlee.wwp@gmail.com>
2018-10-11 22:01:17 +0200

    Add search sub-string history

--- history.zsh
+++ history.zsh
@@ -5,6 +5,14 @@ HISTSIZE=100000
 SAVEHIST=100000
 HISTCONTROL=ignoredups
 
+# Show history
+case $HIST_STAMPS in
+  "mm/dd/yyyy") alias history='fc -fl 1' ;;
+  "dd.mm.yyyy") alias history='fc -El 1' ;;
+  "yyyy-mm-dd") alias history='fc -il 1' ;;
+  *) alias history='fc -l 1' ;;
+esac
+
 setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
@@ -12,4 +20,4 @@ setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
 setopt hist_verify
 setopt inc_append_history
-
+setopt share_history # share command history data
--- keys.zsh
+++ keys.zsh
@@ -21,3 +21,16 @@ bindkey "^[[3~" delete-char
 bindkey "^[3;5~" delete-char
 bindkey "\e[3~" delete-char
 
+# start typing + [Up-Arrow] - fuzzy find history forward
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+  autoload -U up-line-or-beginning-search
+  zle -N up-line-or-beginning-search
+  bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+fi
+# start typing + [Down-Arrow] - fuzzy find history backward
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+  autoload -U down-line-or-beginning-search
+  zle -N down-line-or-beginning-search
+  bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+fi
+
