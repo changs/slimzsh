e347912acebf78b3eb9b1dd16d335a81e80ec7e6
Erlend Nossum <nostra@users.noreply.github.com>
2023-07-27 09:54:20 +0200

    Fix incorrect tab complete suggestion

--- completion.zsh
+++ completion.zsh
@@ -13,7 +13,7 @@ if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
   zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
   unset CASE_SENSITIVE
 else
-  zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+  zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 fi
 
 zstyle ':completion:*' list-colors ''
